78e5cba40f40c1bd402464061717a896ccbeffef
chenxiaoyu <chenxiaoyu3@gmail.com>
2014-12-16 21:47:36 +0800

    s

--- application/views/random.php
+++ application/views/random.php
@@ -1,5 +1,10 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 <style type="text/css">
+	@media (min-width: @screen-sm-min) { 
+		.masthead {
+    		padding: 30px 0;
+    	}
+	 }
 	.cand-list {
         max-width: 300px;
         margin: 0 auto 20px;
@@ -11,39 +16,49 @@
     .result{
     	font-size: 40px;
     }
+    .col-center-block{
+    	float: none;
+    	display: block;
+    	margin: 0 auto;
+    }
+    .masthead {
+    	padding: 70px 0;
+    }
 </style>
 </head>
 
 <body>
-
-	<div class="container">
-		
-		<div class="row">
-			<div class="col-md-12">
-			<div class="input-group">
-				<input id="candi_name" type="text" class="form-control">
-		      	<span class="input-group-btn">
-		        	<button id="add_candidate" class="btn btn-default" type="button">Add</button>
-		      	</span>
-		      	
-		    </div><!-- /input-group -->
-			<table id="tab_candi" class="table table-bordered table-hover">
-				<caption>Now we have</caption>
-				<tbody>
-	
-				</tbody>
-			</table>
-			<a id="toggle" class="btn btn-primary btn-lg btn-block">戳</a>
-			<div id="result_id" class="col-md-12 result text-center">
-			1
+	<header class="navbar"></header>
+	<main class="masthead">
+		<h1 class="text-center"> 戳你妹 </h1>
+		<div class="container">
+			
+			<div class="row">
+				<div class="col-xs-12 col-md-4 col-center-block">
+					<div class="input-group">
+						<input id="candi_name" type="text" class="form-control">
+				      	<span class="input-group-btn">
+				        	<button id="add_candidate" class="btn btn-default" type="button">Add</button>
+				      	</span>
+				      	
+				    </div><!-- /input-group -->
+					<table id="tab_candi" class="table table-bordered table-hover">
+						<caption>Now we have</caption>
+						<tbody>
+						</tbody>
+					</table>
+					
+				</div>
 			</div>
-			<div id="result_name" class="col-md-12 result text-center">
-			xx	
+			<div class="row">
+				<div id="toggle" class="col-xs-8 col-md-1 btn btn-primary btn-lg col-center-block">戳</div>
+				
 			</div>
-			</div>
+			<div id="result_id" class="col-md-12 result text-center"> </div>
+				<div id="result_name" class="col-md-12 result text-center"> </div>
+			
 		</div>
-		
-	</div>
+	</main>
 		<script type="text/javascript">
 		var candidates = new Array();
 		function run(){
