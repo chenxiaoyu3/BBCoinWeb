d35c141cc54afdf0877a0ffb80e66114f205d4dd
chenxiaoyu <chenxiaoyu3@gmail.com>
2014-12-17 19:43:43 +0800

    fix

--- application/views/msg.php
+++ application/views/msg.php
@@ -1,11 +1,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 <link rel="stylesheet" href="<?=asset_url().'css/text_flash.css'?>">
 <style type="text/css">
-	@media (min-width: @screen-sm-min) { 
-		.masthead {
-    		padding: 30px 0;
-    	}
-	 }
+
 	 .lk{
 	 	margin-top: 500px;
 	 }
@@ -17,6 +13,7 @@
 
 <body>
 
+<h1></h1>
 		<h1 class="text-center msg"><?=$msg?></h1>
 
 		<p  class="text-center lk"><a href="<?=base_url().'god'?>">Biu</a></p>
